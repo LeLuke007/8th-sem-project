7c06edd831f71ef4e55baf2a250d62586e0c83ef
Aditya <adityasaini2004@gmail.com>
2024-03-15 00:21:48 +0530

    Added requriements

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask, render_template, request, redirect, send_file, g
-import mysql.connector
+import psycopg2
 import datetime as dt
 from pytz import timezone
 import pandas as pd
 from io import BytesIO
 
 app = Flask(__name__)
-
-'''db_config = {
-    'user': 'lexluke007',
-    'password': 'dbpass123',
-    'host': 'lexluke007.mysql.pythonanywhere-services.com',
-    'database': 'lexluke007$test3'
-}'''
 
 db_config = {
     'dbname': 'verceldb',
@@ -23,29 +16,25 @@ db_config = {
     'sslmode': 'require'
 }
 
-
-con = mysql.connector.connect(**db_config)
-cursor = con.cursor(dictionary=True)
-
 def get_db():
     if 'db' not in g:
-        g.db = mysql.connector.connect(**db_config)
-        g.cursor = g.db.cursor(dictionary=True)
+        g.db = psycopg2.connect(**db_config)
+        g.cursor = g.db.cursor()
     return g.db, g.cursor
 
 def close_db(e=None):
-    cursor = g.pop('cursor', None)
-    if cursor is not None:
-        cursor.close()
-
     db = g.pop('db', None)
     if db is not None:
         db.close()
 
+    cursor = g.pop('cursor', None)
+    if cursor is not None:
+        cursor.close()
+
 def create_table(name):
-    create_table_query = f"CREATE TABLE IF NOT EXISTS {name} (date VARCHAR(255), ref_no1 VARCHAR(255), ref_no2 INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), rollno VARCHAR(255), branch VARCHAR(255), phone VARCHAR(255), email VARCHAR(255), compname VARCHAR(255), comploc VARCHAR(255), pocname VARCHAR(255), pocphone VARCHAR(255))"
-    cursor.execute(create_table_query)
-    con.commit()
+    _, cursor = get_db()
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {name} (date VARCHAR(255), ref_no1 VARCHAR(255), ref_no2 SERIAL PRIMARY KEY, name VARCHAR(255), rollno VARCHAR(255), branch VARCHAR(255), phone VARCHAR(255), email VARCHAR(255), compname VARCHAR(255), comploc VARCHAR(255), pocname VARCHAR(255), pocphone VARCHAR(255))")
+    g.db.commit()
 
 def export_to_excel(table_name):
     data = get_table_entries(table_name)
@@ -55,13 +44,9 @@ def export_to_excel(table_name):
     excel_file.seek(0)
     return excel_file
 
-def send_email(subject, sender, receiver, message):
-    # email sending code here
-    pass
-
 def get_table_entries(table_name):
-    query = f"SELECT * FROM {table_name}"
-    cursor.execute(query)
+    _, cursor = get_db()
+    cursor.execute(f"SELECT * FROM {table_name}")
     data = cursor.fetchall()
     return data
 
@@ -72,15 +57,16 @@ def index():
 
 @app.route('/nocdata/')
 def nocdata():
-    g.db, g.cursor = get_db()
-    cursor.execute("SHOW TABLES")
-    tables = [table['Tables_in_lexluke007$test3'] for table in cursor.fetchall()]
+    _, cursor = get_db()
+    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+    tables = [table[0] for table in cursor.fetchall()]
     return render_template('table.html', tables=tables)
 
 @app.route('/nocdata/', methods=['POST'])
 def display_table_entries():
-    cursor.execute("SHOW TABLES")
-    tables = [table['Tables_in_lexluke007$test3'] for table in cursor.fetchall()]
+    _, cursor = get_db()
+    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+    tables = [table[0] for table in cursor.fetchall()]
     table_name = request.form.get('table_selector')
     try:
         data = get_table_entries(table_name)
@@ -99,11 +85,9 @@ def reg():
         name = request.form['name']
         rollno = request.form['rollno']
         branch = request.form['branch']
-        # email handling code here
         sender_mail = 'lexluke007@gmail.com'
-        receivers_mail = "adityasaini2004@gmail.com"  # for (user in users_db.values() if user.id == faculty[branch])
+        receivers_mail = "adityasaini2004@gmail.com"
         message = f"""From: {sender_mail}\nTo: {receivers_mail}\nSubject: New Application for Registration\n\nThis is a test e-mail message."""
-        send_email("New Application for Registration", sender_mail, receivers_mail, message)
         return render_template('confirmation.html', name=name, message="Your Registration for Eighth Semester is Submitted!")
     return render_template('register.html')
 
@@ -126,17 +110,16 @@ def nocreq():
                       'others': 'Others'}
         faculty = branch_sel[branch]
         certificate = "NOC_" + str(rollno)
-        branch_name = {'bece': 'Electronics and Communication Engineering', 'benc': 'Electronics and Computer Engineering',
-                       'bvlsi': 'Electronics Engineering (VLSI Design and Technology)'}
         create_table(faculty)
+        _, cursor = get_db()
         query = f"INSERT INTO {faculty} (date, ref_no1, name, rollno, branch, phone, email, compname, comploc, pocname, pocphone) " \
                 "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
         values = (date, ref_no1, name, rollno, faculty, phone, email, compname, comploc, pocname, pocphone)
         cursor.execute(query, values)
-        con.commit()
+        g.db.commit()
         export_to_excel(faculty)
         sender_mail = 'lexluke007@gmail.com'
-        receivers_mail = "adityasaini2004@gmail.com"  # for (user in users_db.values() if user.id == faculty[branch])
+        receivers_mail = "adityasaini2004@gmail.com"
         message = f"""From: {sender_mail}\nTo: {receivers_mail}\nSubject: New NOC Application\n\nThis is a test e-mail message."""
         return render_template('confirmation.html', name=name, message="Your NOC Application has been submitted!")
     return render_template("noc.html")
